8d9d34e5ee174ecad73ee3e1c2072f43d2aef7ae
TRUNGNGUYEN\USER <nguyentrung6626@gmail.com>
2017-06-13 22:36:30 +0700

    Giao dien

---
+++ ShopOnline/ShopOnline/Models/BUS/LoaiBUS.cs
@@ -0,0 +1,22 @@
+﻿using ConnectDBShop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopOnline.Models.BUS
+{
+    public class LoaiBUS
+    {
+        public static IEnumerable<LoaiSanPham> DanhSach()
+        {
+            var db = new ConnectDBShopDB();
+            return db.Query<LoaiSanPham>("SELECT * FROM LoaiSanPham WHERE TinhTrang = 0");
+        }
+        public static IEnumerable<SanPham> Chitiet(String id)
+        {
+            var db = new ConnectDBShopDB();
+            return db.Query<SanPham>("SELECT * FROM SanPham WHERE MaLoaiSanPham = '" + id + "'"); //cong chuoi
+        }
+    }
+}
