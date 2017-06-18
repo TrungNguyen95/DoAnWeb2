d234aa065acfa27d56d1c0c201f3f510bd1903e9
DESKTOP-TQ3U4J3\trank <khanhtranhfp@gmail.com>
2017-06-18 14:18:21 +0700

    User, xem danh sách sản phẩm Admin

---
+++ ShopOnline/ShopOnline/Models/BUS/KhachHangBUS.cs
@@ -0,0 +1,17 @@
+﻿using ConnectDBShop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopOnline.Models.BUS
+{
+    public class KhachHangBUS
+    {
+        public static IEnumerable<AspNetUser> DanhSachKH()
+        {
+            var db = new ConnectDBShopDB();
+            return db.Query<AspNetUser>("SELECT * FROM AspNetUsers");
+        }
+    }
+}
